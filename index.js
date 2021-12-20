84db3ee337b6f79554e55d4c05a320eac7bfaa53
Tanmay Mutalik <tanmaymutalik2002@gmail.com>
2021-12-20 08:10:34 +0530

    cors package added to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectToMongoose = require("./database/db");
 
+var cors = require("cors");
 const express = require("express");
 
 connectToMongoose();
@@ -8,6 +9,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", require("./routes/auth.route.js"));
 app.use("/api/product", require("./routes/product.route.js"));
