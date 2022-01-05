399615a6a4cc494e98b09d40e37130f4adf75f17
Tanmay Mutalik <tanmaymutalik2002@gmail.com>
2022-01-05 11:43:13 +0530

    cart and order apis CRUDU completed

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -12,8 +12,20 @@ const response = {
 const getCartItems = async (req, res) => {
   const orderedBy = req.user.id;
   const cartItems = await CartModel.findOne({ orderedBy: orderedBy }).populate(
-    "cartItems",
-    // "orderedBy"
+    // "cartItems"
+    [
+      {
+        path: "cartItems",
+        // select: "productId",
+        populate: {
+          path: "productId",
+          model: "product",
+        },
+      },
+      // {
+      //   path: "orderedBy",
+      // },
+    ]
   );
   res.send(cartItems);
 };
@@ -128,39 +140,211 @@ const addCartItems = async (req, res) => {
             response.success = true;
             response.message = "Cart updated succesfully";
             console.log(response);
-            res.json(result);
+            res.status(200).send(result);
           } else {
-            res.send("Result not found 2");
+            res.status(404).send("Result not found 2");
           }
         })
         .catch((error) => {
           response.success = false;
           response.message = "Some error occured";
           console.log(response);
-          res.send(error.message);
+          res.status(500).send(error.message);
         });
     }
-    // to buyCart Items
-    // await OrderModel.create({
-    //   orderedBy: orderedBy,
-    //   orderItems: orderArray,
-    //   address: address,
-    // })
-    //   .then((result) => {
-    //     if (result) {
-    //       response.success = true;
-    //       response.message = "Order added succesfully";
-    //       console.log(response);
-    //       res.status(200).send(result);
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     response.success = false;
-    //     response.message = "Some error occured";
-    //     console.log(response);
-    //     res.send(error.message);
-    //   });
   }
 };
 
-module.exports = { addCartItems, getCartItems };
+const updateCartItem = async (req, res) => {
+  const orderedBy = req.user.id;
+  const { cartItemId } = req.params;
+  const { productQty } = req.body;
+  await OrderItemModel.findOne({ _id: cartItemId })
+    .then(async (cartItem) => {
+      if (cartItem.orderedBy != orderedBy) {
+        response.success = false;
+        response.message = "User can update only own order items";
+        return res.status(404).send("Action not allowed");
+      }
+      await OrderItemModel.findOneAndUpdate(
+        { _id: cartItemId },
+        { $set: { productQty: productQty } },
+        { $new: true }
+      )
+        .then((result) => {
+          response.success = true;
+          response.message = "Cart item updated succesfully";
+          console.log(response);
+          res.json({ message: "Updated Cart Item", cartItem: result });
+        })
+        .catch((err) => {
+          response.success = false;
+          response.message = "Some error occured";
+          return res.status(500).send(err.message);
+        });
+    })
+    .catch((err) => {
+      response.success = false;
+      response.message = "Some error occured";
+      return res.status(500).send(err.message);
+    });
+};
+
+const deleteCartItem = async (req, res) => {
+  const orderedBy = req.user.id;
+  const { cartItemId } = req.params;
+  await OrderItemModel.findOne({ _id: cartItemId })
+    .then(async (cartItem) => {
+      if (cartItem.orderedBy != orderedBy) {
+        response.success = false;
+        response.message = "User can delete only own order items";
+        return res.status(404).send("Action not allowed");
+      }
+      await OrderItemModel.findOneAndDelete({ _id: cartItemId })
+        .then((result) => {
+          response.success = true;
+          response.message = "Cart item deleted succesfully";
+          console.log(response);
+          res.json({ message: "Deleted Cart Item", deletedCartItem: result });
+        })
+        .catch((err) => {
+          response.success = false;
+          response.message = "Some error occured";
+          return res.status(500).send(err.message);
+        });
+    })
+    .catch((err) => {
+      response.success = false;
+      response.message = "Some error occured";
+      return res.status(500).send(err.message);
+    });
+};
+
+const getOrders = async (req, res) => {
+  const orderedBy = req.user.id;
+  await OrderModel.find({ orderedBy: orderedBy })
+    .then((result) => {
+      if (result) {
+        response.success = true;
+        response.message = "Cart updated succesfully";
+        console.log(response);
+        res.status(200).send({ numOfOrders: result.length, orders: result });
+      } else {
+        response.success = null;
+        response.message = "No Orders found";
+        console.log(response);
+        res.status(200).send("No orders found");
+      }
+    })
+    .catch((err) => {
+      response.success = false;
+      response.message = "Some error occured";
+      console.log(response);
+      res.send(error.message);
+    });
+};
+
+const placeOrder = async (req, res) => {
+  const orderedBy = req.user.id;
+  const { address } = req.body;
+  await CartModel.findOne({ orderedBy: orderedBy })
+    .then(async (result) => {
+      if (!result) {
+        response.success = false;
+        response.message = "Cart is empty";
+        console.log(response);
+        res.send("Cart is empty");
+      } else {
+        await OrderModel.create({
+          orderedBy: orderedBy,
+          orderItems: result.cartItems,
+          address: address,
+        })
+          .then(async (result) => {
+            if (result) {
+              response.success = true;
+              response.message = "Order placed succesfully";
+              console.log(response);
+              res.status(200).send(result);
+              await CartModel.findOneAndDelete({ orderedBy: orderedBy })
+                .then((result) => {
+                  response.success = true;
+                  response.message = "Cart truncated successfully";
+                  console.log(response);
+                })
+                .catch((error) => {
+                  response.success = false;
+                  response.message = "Some error occured";
+                  console.log(response);
+                });
+            }
+          })
+          .catch((error) => {
+            response.success = false;
+            response.message = "Some error occured";
+            console.log(response);
+            res.send(error.message);
+          });
+      }
+    })
+    .catch((err) => {
+      response.success = false;
+      response.message = "Some error occured";
+      console.log(response);
+      res.send(err.message);
+    });
+};
+
+const cancelOrder = async (req, res) => {
+  const orderedBy = req.user.id;
+  const { cartId } = req.params;
+  await OrderModel.findOne({ _id: cartId })
+    .then(async (result) => {
+      if (result) {
+        if (result.orderedBy != orderedBy) {
+          response.success = false;
+          response.message = "Cannot delete other's order";
+          console.log(response);
+          res.send("Action not allowed");
+        } else {
+          await OrderModel.findOneAndDelete({ _id: cartId })
+            .then((result) => {
+              response.success = true;
+              response.message = "Order Cancelled";
+              console.log(response);
+              res.status(200).send({
+                message: "Order Cancelled",
+                deletedCart: result,
+              });
+            })
+            .catch((error) => {
+              response.success = false;
+              response.message = "Some error occured";
+              console.log(response);
+              res.status(500).send(error.message);
+            });
+        }
+      } else {
+        response.success = false;
+        response.message = "Order not found";
+        console.log(response);
+        res.status(404).send("Order not found");
+      }
+    })
+    .catch((err) => {
+      response.success = false;
+      response.message = "Some error occured";
+      console.log(response);
+      res.send(err.message);
+    });
+};
+
+module.exports = {
+  addCartItems,
+  getCartItems,
+  updateCartItem,
+  deleteCartItem,
+  placeOrder,
+  cancelOrder,
+  getOrders,
+};
--- routes/order.route.js
+++ routes/order.route.js
@@ -3,10 +3,20 @@ const router = express.Router();
 const {
   addCartItems,
   getCartItems,
+  updateCartItem,
+  deleteCartItem,
+  placeOrder,
+  cancelOrder,
+  getOrders,
 } = require("../controllers/order.controller.js");
 const fetchuser = require("../middlewares/fetchuser.middleware");
 
 router.post("/add-cart-items", fetchuser, addCartItems);
 router.get("/getcart", fetchuser, getCartItems);
+router.put("/update-cart-item/:cartItemId", fetchuser, updateCartItem);
+router.delete("/delete-cart-item/:cartItemId", fetchuser, deleteCartItem);
+router.post("/my-orders", fetchuser, getOrders);
+router.post("/place-order", fetchuser, placeOrder);
+router.delete("/cancel-order/:cartId", fetchuser, cancelOrder);
 
 module.exports = router;
